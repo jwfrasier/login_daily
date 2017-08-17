177b784575a09ae71f66c51d36c86e37d3ac3f7e
Joseph Frasier <frasier.joseph@gmail.com>
2017-08-17 17:17:11 -0500

    daily login

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mustache = require("mustache-express")
 const bodyParser = require("body-parser")
 const url = require("url")
 const session = require("express-session")
-const users = require("./users")
+const users = require("./testusers")
 
 app.engine("mustache", mustache())
 app.set("view engine", "mustache")
@@ -22,7 +22,8 @@ var userSess = {
   saveUninitialized: true,
   resave: true
 }
-
+// calling to use the encryption var we declared
+app.use(session(userSess))
 // This will parse the url if it is entered in uppercase
 // and will just redirect to the page with lowercase char
 app.use(function(req, res, next) {
@@ -34,11 +35,59 @@ app.use(function(req, res, next) {
     next()
   }
 })
-app.use(session(userSess))
-
-// Render the home page
+// Set the user automatically to a false in session and redirect to
+// the login page.
 app.get("/", function(req, res, next) {
+  console.log(req.session)
+  // if req.session == truthy
+  // render index
+  // else, redirect to login
+  req.session.authorized = false
+  res.redirect("/login")
+})
 
+// Render login page first
+app.get("/login", function(req, res, next) {
+  res.render("login")
+})
+
+// Authenticate what we got from the form and compare it to your array
+// of users.  Redirect to login if failed.
+app.post("/authorization", function(req, res, ) {
+
+  const loginName = req.body.username
+  const passwordInfo = req.body.password
+
+  let database
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].username === loginName && users[i].password === passwordInfo) {
+      database = users[i]
+    }
+  }
+  if (database) {
+    req.session.user = database
+    req.session.authorized = true
+    res.redirect("/index")
+  } else {
+    res.render("login", {
+      errorMessage: "Login Incorrect, please verify your username and password."
+    })
+  }
+})
+// Store the current session user into req.user to be used later
+
+app.use(function(req, res, next) {
+  req.user = req.session.user
+  next()
+})
+
+// push the information to index
+app.get("/index", function(req, res, next) {
+  const currentUser = req.user
+  res.render("index", {
+    currentUser: currentUser
+
+  })
 })
 
 app.listen(3000, function() {
---
+++ testusers.js
@@ -0,0 +1,27 @@
+const users = [{
+    username: "joseph",
+    password: "cake"
+
+  },
+  {
+    username: "david",
+    password: "pie"
+
+  },
+  {
+    username: "bob",
+    password: "brownie"
+
+  },
+  {
+    username: "suzie",
+    password: "lemon"
+
+  },
+  {
+    username: "billy",
+    password: "swag"
+
+  },
+]
+module.exports = users
